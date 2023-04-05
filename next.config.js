b5b17f3e1004312b42c78fd620f56044d57b86bf
0xRenegade <therenegadecoding@gmail.com>
2023-04-05 01:30:55 -0500

    dynamic proxy for local vs deployed with subdomain

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+let path = '/api/:path*'
+
+if (!process.env.NEXT_PUBLIC_ENVIRONMENT === 'local') {
+  path = '/:path*'
+}
+
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_BACKEND_URL + path,
+      },
+    ]
+  },
+  reactStrictMode: false,
   experimental: {
     appDir: true,
   },
