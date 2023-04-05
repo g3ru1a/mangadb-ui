997ed94d45c0e2ce9269b06df8374628ebbca042
0xRenegade <therenegadecoding@gmail.com>
2023-04-05 11:50:11 -0500

    add logout route

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import axios from '~/lib/axios'
+
+export default function Logout() {
+  const router = useRouter()
+
+  useEffect(() => {
+    localStorage.removeItem('auth')
+
+    const logout = async () => {
+      try {
+        const res = await axios.post('/api/logout')
+        console.log(res)
+      } catch (err: any) {
+        console.log(err)
+      }
+    }
+    logout()
+  })
+
+  router.push('/')
+}
