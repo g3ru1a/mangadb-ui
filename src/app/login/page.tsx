028fa061eea3c0c9e1c41bff466be97c617d0b14
0xRenegade <therenegadecoding@gmail.com>
2023-04-06 12:41:17 -0500

    style form using tailwind

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -27,23 +27,55 @@ export default function Login() {
     } catch (err: any) {
       console.log(err)
       if (err.response.status === 404) console.log('handle user not found')
-      if (err.response.status === 422) console.log('handle bad username or password')
+      if (err.response.status === 422)
+        console.log('handle bad username or password')
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input type="email" name="email" onChange={handleInputChange} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" name="password" onChange={handleInputChange} />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="flex justify-center items-center h-screen">
+      <form
+        className="bg-white p-6 rounded-lg shadow-md"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-2xl font-medium mb-4">Log in</h2>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 font-medium mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="border border-gray-400 p-2 w-full rounded-md"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 font-medium mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="border border-gray-400 p-2 w-full rounded-md"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange}
+          />
+        </div>
+        <div className="flex justify-center">
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+            type="submit"
+          >
+            Log in
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
