0fcf080693405c138a0045bc13c3330630a8ce72
0xRenegade <therenegadecoding@gmail.com>
2023-04-06 15:06:01 -0500

    unused imports

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import {  useState } from 'react'
 import { toast } from 'react-toastify'
 import axios from '~/lib/axios'
 
