9a269679d292875767da8d53d3a499dcdc2892a2
0xRenegade <therenegadecoding@gmail.com>
2023-04-14 07:24:31 -0500

    remove proxy

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-let path = '/api/:path*'
-
-if (process.env.NEXT_PUBLIC_ENVIRONMENT !== 'local') {
-  path = '/:path*'
-}
-
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_BACKEND_URL + path,
-      },
-    ]
-  },
   reactStrictMode: false,
   experimental: {
     appDir: true,
